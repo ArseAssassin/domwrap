cd82fabe66904fe2771c4f784da0897ca4d07e0b
ArseAssassin <tuomas@kanerva.info>
2011-12-14 14:29:48 +0200

    initial import

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="DOMWrap",
+	version="0.1",
+	description="Simple wrapper for minidom to make traversing easier",
+	author="Tuomas Kanerva",
+	packages=['domwrap']
+)
